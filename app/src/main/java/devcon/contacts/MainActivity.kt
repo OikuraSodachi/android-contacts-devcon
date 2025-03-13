54ed7faf8d8e0d7714f93caaecdc96e43bb3db2d
Todokanai <bh951201@gmail.com>
2025-03-13 09:41:09 +0900

    feat($MainActivity): add features
    -이름과 전화번호 미입력시 토스트 메시지
    -더보기 클릭으로 입력 폼을 확장 가능(생일, 성별, 메모 입력 폼)
    -저장/취소 버튼 클릭시 토스트 메시지

--- app/src/main/java/devcon/contacts/MainActivity.kt
+++ app/src/main/java/devcon/contacts/MainActivity.kt
@@ -1,27 +1,53 @@
 package devcon.contacts
 
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import devcon.learn.contacts.R
 
 class MainActivity : AppCompatActivity() {
 
-    private val cancelButton by lazy {findViewById<Button>(R.id.cancelBtn)}
-    private val saveButton by lazy {findViewById<Button>(R.id.saveBtn)}
+    private val cancelButton by lazy { findViewById<Button>(R.id.cancelBtn) }
+    private val saveButton by lazy { findViewById<Button>(R.id.saveBtn) }
+    private val nameInputText by lazy { findViewById<TextInputEditText>(R.id.nameInputEditText) }
+    private val contactInputText by lazy{ findViewById<TextInputEditText>(R.id.contactInputEditText) }
+    private val moreBtn by lazy{ findViewById<Button>(R.id.moreBtn) }
+    private val genderInputText by lazy{ findViewById<TextInputEditText>(R.id.genderInputEditText) }
+    private val genderSelection by lazy{ findViewById<RadioGroup>(R.id.genderSelection)}
+    private val optionalLayout by lazy{ findViewById<LinearLayout>(R.id.optionalLayout) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        saveButton.setOnClickListener {
-            Toast.makeText(this,"저장이 완료 되었습니다",Toast.LENGTH_SHORT).show()
-
+        saveButton.setOnClickListener { onSave() }
+        cancelButton.setOnClickListener { toast("취소 되었습니다") }
+        moreBtn.setOnClickListener {
+            moreBtn.visibility = GONE
+            optionalLayout.visibility = VISIBLE
         }
-        cancelButton.setOnClickListener {
-            Toast.makeText(this,"저장이 완료 되었습니다",Toast.LENGTH_SHORT).show()
 
+        genderSelection.setOnCheckedChangeListener { _, selected ->
+            genderInputText.setText(findViewById<RadioButton>(selected).text)
         }
     }
+
+    fun onSave(){
+        val name = nameInputText.text.isNullOrEmpty()
+        val contact = contactInputText.text.isNullOrEmpty()
+        if(!name && !contact){
+            toast("저장이 완료 되었습니다")
+        }else{
+            toast("이름, 전화번호는 필수 항목입니다")
+        }
+    }
+
+    fun toast(text:String) = Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
 }
