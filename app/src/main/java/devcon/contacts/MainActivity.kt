86f0a2b8371bf86b97cd76085261ac1ab07ce0f9
Todokanai <bh951201@gmail.com>
2025-03-15 07:31:12 +0900

    style($MainActivity.kt, $activity_main.xml)

--- app/src/main/java/devcon/contacts/MainActivity.kt
+++ app/src/main/java/devcon/contacts/MainActivity.kt
@@ -14,14 +14,14 @@ import devcon.learn.contacts.R
 
 class MainActivity : AppCompatActivity() {
 
-    private val cancelButton by lazy { findViewById<Button>(R.id.cancelBtn) }
-    private val saveButton by lazy { findViewById<Button>(R.id.saveBtn) }
+    private val cancelButton by lazy { findViewById<Button>(R.id.cancelButton) }
+    private val saveButton by lazy { findViewById<Button>(R.id.saveButton) }
     private val nameInputText by lazy { findViewById<TextInputEditText>(R.id.nameInputEditText) }
-    private val contactInputText by lazy{ findViewById<TextInputEditText>(R.id.contactInputEditText) }
-    private val moreBtn by lazy{ findViewById<Button>(R.id.moreBtn) }
-    private val genderInputText by lazy{ findViewById<TextInputEditText>(R.id.genderInputEditText) }
-    private val genderSelection by lazy{ findViewById<RadioGroup>(R.id.genderSelection)}
-    private val optionalLayout by lazy{ findViewById<LinearLayout>(R.id.optionalLayout) }
+    private val contactInputText by lazy { findViewById<TextInputEditText>(R.id.contactInputEditText) }
+    private val moreButton by lazy { findViewById<Button>(R.id.moreButton) }
+    private val genderInputText by lazy { findViewById<TextInputEditText>(R.id.genderInputEditText) }
+    private val genderSelection by lazy { findViewById<RadioGroup>(R.id.genderSelection) }
+    private val optionalLayout by lazy { findViewById<LinearLayout>(R.id.optionalLayout) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +29,8 @@ class MainActivity : AppCompatActivity() {
 
         saveButton.setOnClickListener { onSave() }
         cancelButton.setOnClickListener { toast("취소 되었습니다") }
-        moreBtn.setOnClickListener {
-            moreBtn.visibility = GONE
+        moreButton.setOnClickListener {
+            moreButton.visibility = GONE
             optionalLayout.visibility = VISIBLE
         }
 
@@ -39,15 +39,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onSave(){
+    fun onSave() {
         val name = nameInputText.text.isNullOrEmpty()
         val contact = contactInputText.text.isNullOrEmpty()
-        if(!name && !contact){
+        if (!name && !contact) {
             toast("저장이 완료 되었습니다")
-        }else{
+        } else {
             toast("이름, 전화번호는 필수 항목입니다")
         }
     }
 
-    fun toast(text:String) = Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+    fun toast(text: String) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
